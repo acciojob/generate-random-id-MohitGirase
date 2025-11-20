64cd26820b3872f464e09cea2590349252b6e884
Mohit Girase <114944481+MohitGirase@users.noreply.github.com>
2025-11-20 15:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
-  // write your code here
+	// write your code here
+	const char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	const len = char_list.length;
+
+	let ans = "";
+	for(let i=0; i<l; i++){
+		ans += char_list.charAt(
+			Math.floor(Math.random()*len);
+		)
+	}
+	return ans;
 }
 
 // Do not change the code below.
